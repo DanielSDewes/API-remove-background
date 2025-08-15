721948bd8ea7c634b3e4de700c8e3732cc3c6ed8
DanielD <danielsilvadewe@gmail.com>
2025-08-15 20:42:21 -0300

    liberando todas as origens para teste

--- app/main.py
+++ app/main.py
@@ -9,14 +9,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Remove Background API")
 
-origins = [
-    "https://remove-bg-steel.vercel.app",
-    "http://localhost:3000"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
